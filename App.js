8f4c077ee1e5c2f62ac98a49bbbe50ae5ae63c62
hemnanilakshya <122461965+hemnanilakshya@users.noreply.github.com>
2024-07-13 11:35:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Calculator from "./components/Calculator";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my First React Application</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
